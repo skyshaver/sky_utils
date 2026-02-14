160b09357dc1b1d04d05fd6ddc6c352e7da6f4c2
Sky Shaver <skyshaver@me.com>
2026-02-13 18:31:21 -0800

    add tests for strtoint64t

--- tests/sky_utils_test.cpp
+++ tests/sky_utils_test.cpp
@@ -1,9 +1,25 @@
 #include <gtest/gtest.h>
 #include "../sky_utils.hpp"
 
-
+// Both names must be valid C++ identifiers, and they should not contain any underscores (_)
 TEST(StringToInt64t, ReturnsValidInt64t) {
     EXPECT_EQ(sky_utils::to_int64("5678"), 5678);
+}
+
+TEST(StringToInt64t, HandlesEmptyString) {
+    EXPECT_EQ(sky_utils::to_int64(""), std::nullopt);
+}
+
+TEST(StringToInt64t, HandlesInvalidString) {
+    EXPECT_EQ(sky_utils::to_int64("bob"), std::nullopt);
+}
+
+TEST(StringToInt64t, HandlesMaxValue) {
+    EXPECT_EQ(sky_utils::to_int64("9223372036854775807"), std::numeric_limits<int64_t>::max());
+}
+
+TEST(StringToInt64t, HandlesMinValue) {
+    EXPECT_EQ(sky_utils::to_int64("-9223372036854775808"), std::numeric_limits<int64_t>::min());
 }
 
 int main(int argc, char** argv) {
